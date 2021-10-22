bdad600d90f4815c0831e8b9c490ddb8888c414f
Shirish Joshi <shirish.joshi@gmail.com>
2021-10-22 11:39:45 +0000

    test

---
+++ main.tf
@@ -0,0 +1,12 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "shirish-app-server1" {
+  ami           = "ami-02e136e904f3da870"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "shirish-server-new-tf"
+  }
+}
--- sj
+++
@@ -1,2 +0,0 @@
-perfect sj
-correct sj
